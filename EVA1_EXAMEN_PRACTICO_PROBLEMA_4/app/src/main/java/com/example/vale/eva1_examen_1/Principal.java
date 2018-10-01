48e8a55509962386589821e072a7c6ba9d3cbc2c
Jonathan Mata <jona622@hotmail.com>
2018-09-30 23:52:31 -0600

    Fix examen 4

--- EVA1_EXAMEN_PRACTICO_PROBLEMA_4/app/src/main/java/com/example/vale/eva1_examen_1/Principal.java
+++ EVA1_EXAMEN_PRACTICO_PROBLEMA_4/app/src/main/java/com/example/vale/eva1_examen_1/Principal.java
@@ -3,6 +3,7 @@ package com.example.vale.eva1_examen_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -67,22 +68,69 @@ public class Principal extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 int deci1 = Integer.parseInt(edT1.getText().toString());
-                int  deci2 = Integer.parseInt(edT2.getText().toString());
-                int  deci3 = Integer.parseInt(edT3.getText().toString());
-                int  deci4 = Integer.parseInt(edT4.getText().toString());
-                int  deci5 = Integer.parseInt(edT5.getText().toString());
+                int deci2 = Integer.parseInt(edT2.getText().toString());
+                int deci3 = Integer.parseInt(edT3.getText().toString());
+                int deci4 = Integer.parseInt(edT4.getText().toString());
+                int deci5 = Integer.parseInt(edT5.getText().toString());
                 int deci6 = Integer.parseInt(edT6.getText().toString());
-                int  deci7 = Integer.parseInt(edT7.getText().toString());
-                int  deci8 = Integer.parseInt(edT8.getText().toString());
-                String bin = Integer.toBinaryString(deci1);
-                String bin2 = Integer.toBinaryString(deci2);
-                String bin3 = Integer.toBinaryString(deci3);
-                String bin4 = Integer.toBinaryString(deci4);
-                String bin5 = Integer.toBinaryString(deci5);
-                String bin6 = Integer.toBinaryString(deci6);
-                String bin7 = Integer.toBinaryString(deci7);
-                String bin8 = Integer.toBinaryString(deci8);
+                int deci7 = Integer.parseInt(edT7.getText().toString());
+                int deci8 = Integer.parseInt(edT8.getText().toString());
+                String bin1 = convert2Binary(deci1);
+                String bin2 = convert2Binary(deci2);
+                String bin3 = convert2Binary(deci3);
+                String bin4 = convert2Binary(deci4);
+                String bin5 = convert2Binary(deci5);
+                String bin6 = convert2Binary(deci6);
+                String bin7 = convert2Binary(deci7);
+                String bin8 = convert2Binary(deci8);
+                String newOct1 = "", newOct2 = "", newOct3 = "", newOct4 = "";
+                int oct1 = 0, oct2 = 0, oct3 = 0, oct4 = 0;
+
                 int prefix = 0;
+                String mask = bin1+bin2+bin3+bin4;
+                Log.i("Mask", mask);
+                for (int i=1; i < mask.length(); i++){
+                    if(mask.charAt(i) == 1 && mask.charAt(i-1) == 1){
+                        prefix++;
+                    }
+                }
+
+                for (int i = 0; i < 8; i++){
+                    if(bin1.charAt(i) == 1 && bin1.charAt(i) == bin5.charAt(i)){
+                        newOct1 += "1";
+                    }else{
+                        newOct1 += "0";
+                    }
+                }
+                for (int i = 0; i < 8; i++){
+                    if(bin2.charAt(i) == 1 && bin2.charAt(i) == bin6.charAt(i)){
+                        newOct2 += "1";
+                    }else{
+                        newOct2 += "0";
+                    }
+                }
+                for (int i = 0; i < 8; i++){
+                    if(bin3.charAt(i) == 1 && bin3.charAt(i) == bin7.charAt(i)){
+                        newOct3 += "1";
+                    }else{
+                        newOct3 += "0";
+                    }
+                }
+                for (int i = 0; i < 8; i++){
+                    if(bin4.charAt(i) == 1 && bin4.charAt(i) == bin8.charAt(i)){
+                        newOct4 += "1";
+                    }else{
+                        newOct4 += "0";
+                    }
+                }
+
+                oct1 = Integer.parseInt(newOct1, 2);
+                oct2 = Integer.parseInt(newOct2, 2);
+                oct3 = Integer.parseInt(newOct3, 2);
+                oct4 = Integer.parseInt(newOct4, 2);
+
+                TxtVSub.setText("Subred: " + oct1 + "." + oct2 + "." + oct3 + "." + oct4);
+                TxtVPre.setText("Prefijo: /" + prefix);
             }
 
         });
@@ -96,13 +144,13 @@ public class Principal extends AppCompatActivity
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) { //int es el id del boton que se presiono
         int deci1 = Integer.parseInt(edT1.getText().toString());
-        int  deci2 = Integer.parseInt(edT2.getText().toString());
-        int  deci3 = Integer.parseInt(edT3.getText().toString());
-        int  deci4 = Integer.parseInt(edT4.getText().toString());
-        int  deci5 = Integer.parseInt(edT5.getText().toString());
+        int deci2 = Integer.parseInt(edT2.getText().toString());
+        int deci3 = Integer.parseInt(edT3.getText().toString());
+        int deci4 = Integer.parseInt(edT4.getText().toString());
+        int deci5 = Integer.parseInt(edT5.getText().toString());
         int deci6 = Integer.parseInt(edT6.getText().toString());
-        int  deci7 = Integer.parseInt(edT7.getText().toString());
-        int  deci8 = Integer.parseInt(edT8.getText().toString());
+        int deci7 = Integer.parseInt(edT7.getText().toString());
+        int deci8 = Integer.parseInt(edT8.getText().toString());
         switch (checkedId) {
             case R.id.rbDe://aqui se sabe cual boton pico
                 Des = "Decimal";
@@ -111,18 +159,31 @@ public class Principal extends AppCompatActivity
                 break;
             case R.id.rbBi:
                 Des = "Binario";
-                String bin = Integer.toBinaryString(deci1);
-                String bin2 = Integer.toBinaryString(deci2);
-                String bin3 = Integer.toBinaryString(deci3);
-                String bin4 = Integer.toBinaryString(deci4);
-                String bin5 = Integer.toBinaryString(deci5);
-                String bin6 = Integer.toBinaryString(deci6);
-                String bin7 = Integer.toBinaryString(deci7);
-                String bin8 = Integer.toBinaryString(deci8);
-                TxtVIP.setText("IP  " + bin + ",  " + bin2 + ",  " + bin3 + ",  " + bin4 );
+                String bin1 = convert2Binary(deci1);
+                String bin2 = convert2Binary(deci2);
+                String bin3 = convert2Binary(deci3);
+                String bin4 = convert2Binary(deci4);
+                String bin5 = convert2Binary(deci5);
+                String bin6 = convert2Binary(deci6);
+                String bin7 = convert2Binary(deci7);
+                String bin8 = convert2Binary(deci8);
+                TxtVIP.setText("IP  " + bin1 + ",  " + bin2 + ",  " + bin3 + ",  " + bin4 );
                 TxtVMa.setText("Mask  " + bin5 + ",  " + bin6 + ",  " + bin7 + ",  " + bin8 );
                 break;
         }
+    }
+
+    public String convert2Binary(int num){
+        String bin = Integer.toBinaryString(num);
+        if(bin.length() < 8){
+            int rest = 8 - bin.length();
+            String r = "";
+            for (int i = 0; i<rest; i++){
+                r += "0";
+            }
+            bin  = r+bin;
+        }
+        return bin;
     }
 
 }
